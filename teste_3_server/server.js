c7b8c5ea59f63fc922a1138b34cd1b2c874b82e3
Wellington S. <58373332+well1ngt0nso@users.noreply.github.com>
2024-10-30 23:21:37 -0300

    Add files via upload

---
+++ teste_3_server/server.js
@@ -0,0 +1,32 @@
+const { createServer } = require('node:http');
+const port = 3000;
+const localhost = '127.0.0.1';
+
+server = createServer((req,  res) => {
+
+    if(req.url === "/pasta"){
+        res.statusCode = 200;
+        res.setHeader('Contain-Type', 'text/plain');
+        res.end(req.url);
+        console.log(req.url)
+
+    } else if(req.url === "/edit"){
+
+        res.statusCode = 200;
+        res.setHeader('Contain-Type', 'text/plain');
+        res.end(req.url);
+        console.log(req.url)
+
+    }else{
+        res.statusCode = 202;
+        res.setHeader('Contain-Type', 'text/plain');
+        res.end(`Não encontrado`);
+        console.log(req.url)
+
+    }
+})
+server.listen(port, localhost, ()=>{
+
+    console.log("Server conectado");
+
+});
